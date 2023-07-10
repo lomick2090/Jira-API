08b75784d43058590e05bb7590fed97c7352f5da
lomick2090 <lomick2090@gmail.com>
2023-07-10 17:19:35 -0500

    Pull api data

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,34 @@
-var axios = require('axios');
+//import getIssuesByProject from './apiFunctions/getIssuesByProject'
+//import {callAppendRows} from './bigQueryFunctions/uploadIssues'
 require('dotenv').config();
 
 const username: string | undefined = process.env.ATLASSIAN_USERNAME
 const password: string | undefined  = process.env.ATLASSIAN_API_KEY
 const domain: string | undefined  = process.env.DOMAIN
-const projectKey: string | undefined = process.env.PROJECT_KEY
 
-//Gets all issues in a particular project using the Jira Cloud REST API
+//getIssuesByProject('IT').then((res: any) => console.log(res))
 
-const bodyData = `{
-    "fields": [
-        "summary",
-        "status",
-        "assignee",
-        "timespent",
-        "priority",
-        "created",
-        "creator",
-    ],
-    "jql": "project = ${projectKey}"
-}`
 
-async function getIssues() {
-    fetch(`https://${domain}.atlassian.net/rest/api/3/search`, {
-        method: 'POST',
-        headers: {
-            'Authorization': `Basic ${Buffer.from(
-            `${username}:${password}`
-            ).toString('base64')}`,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: bodyData
-    })
-    .then(response => {
-        console.log(
-        `Response: ${response.status} ${response.statusText}`
-        );
-        return response.text();
-    })
-    .then(text => console.log(text))
-    .catch(err => console.error(err));
+export async function callAppendRows() {
+    const {BigQuery} = require('@google-cloud/bigquery');
+    const bigquery = new BigQuery();
+    const dataset = bigquery.dataset('institutions');
+
+    const tableId = 'institution_data';
+
+    const options = {
+        schema: 'PROJECT,ID,SUMMARY,DESCRIPTION,ASSIGNEE_NAME,ASSIGNEE_ID,REPORTER_NAME,REPORTER_ID,PRIORITY,CREATED,DUE,TIMESPENT'
+    };
+
+    dataset.createTable(tableId, options, (err: any, table: any, apiResponse: any) => {});
+
+    //-
+    // If the callback is omitted, we'll return a Promise.
+    //-
+    dataset.createTable(tableId, options).then((data: any) => {
+        const table = data[0];
+        const apiResponse = data[1];
+    });
 }
 
-getIssues()
+callAppendRows();
